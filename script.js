3a92cef6717fa7159373a632d6c3a2d616e6431b
Mark Gatere <gateremark5@gmail.com>
2022-12-12 19:16:57 +0300

    Added new files

---
+++ script.js
@@ -0,0 +1,120 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#4bad2e';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('h1').style.color = '#030303';
+    document.querySelector('.footer').style.color = '#030303';
+    document.querySelector('.label-score').style.color = '#030303';
+    document.querySelector('.between').style.color = '#030303';
+    document.querySelector('.label-highscore').style.color = '#030303';
+    document.querySelector('.guess').style.borderColor = '#030303';
+    document.querySelector('.guess').style.color = '#030303';
+    document.querySelector('.check').style.backgroundColor = '#030303';
+    document.querySelector('.check').style.color = '#fff';
+    document.querySelector('.again').style.backgroundColor = '#030303';
+    document.querySelector('.again').style.color = '#fff';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#030303';
+  document.querySelector('.number').style.width = '15rem';
+
+  //Newly added by Me
+
+  document.querySelector('h1').style.color = '#fff';
+  document.querySelector('.footer').style.color = '#fff';
+  document.querySelector('.label-score').style.color = '#fff';
+  document.querySelector('.between').style.color = '#fff';
+  document.querySelector('.label-highscore').style.color = '#fff';
+  document.querySelector('.guess').style.borderColor = '#fff';
+  document.querySelector('.guess').style.color = '#fff';
+  document.querySelector('.check').style.backgroundColor = '#fff';
+  document.querySelector('.check').style.color = '#030303';
+  document.querySelector('.again').style.backgroundColor = '#fff';
+  document.querySelector('.again').style.color = '#030303';
+});
